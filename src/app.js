43fad6ea9516410c1bf6872f9a3b16d04ac0b523
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-05-08 08:14:39 -0300

    build: atlas, and render

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(pollRouter)
 app.use(choiceRouter)
 
 
-// PORT 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
